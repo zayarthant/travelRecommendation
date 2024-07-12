bc8a25bb6911689ebe35c8953d75b840bada9617
Zayar Thant <zayarthant.deca@gmail.com>
2024-07-12 22:43:01 +0700

    just finish few more

--- script/travel_recommendation.js
+++ script/travel_recommendation.js
@@ -1,4 +1,4 @@
-async function fetchTravelRecommendations() {
+async function fetchTravelRecommendations(keyword) {
   try {
     const response = await fetch('res/travel_recommendation_api.json');
     if (!response.ok) {
@@ -11,4 +11,39 @@ async function fetchTravelRecommendations() {
   }
 }
 
-fetchTravelRecommendations().then(r => console.log('Ended'));
+function searchRec() {
+  const inputElement = document.getElementById('searchInput');
+  const inputValue = inputElement.value;
+  const keyword = inputValue.toLowerCase();
+  inputElement.value = keyword;
+
+  document.getElementById('recCC').innerHTML = '';
+
+  fetchTravelRecommendations(keyword).then(r => console.log('Ended'));
+
+}
+
+function clearSearch(){
+  const inputElement = document.getElementById('searchInput');
+  inputElement.value = '';
+}
+
+function addRec(image, title, context) {
+  var outerDiv = document.createElement('div');
+  outerDiv.className = 'w3-white w3-card-2 w3-round-large w3-padding-small w3-margin-top';
+  var img = document.createElement('img');
+  img.className = 'w3-image';
+  img.style.width = '100%';
+  img.src = image;
+  outerDiv.appendChild(img);
+  var innerDiv = document.createElement('div');
+  var h3 = document.createElement('h3');
+  h3.textContent = title;
+  innerDiv.appendChild(h3);
+  var p = document.createElement('p');
+  p.className = 'w3-justify';
+  p.textContent = context;
+  innerDiv.appendChild(p);
+  outerDiv.appendChild(innerDiv);
+  document.getElementById('recCC').appendChild(outerDiv);
+}
